ef97d43e7f303bcb891c12eb52b055e56bea7482
MehediIslamRipon <mehediislamripon@gmail.com>
2022-08-14 18:03:36 +0600

    learned about complex state management using useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Counter from './Components/Counter';
+import Stats from './Components/Stats';
+
+const initialState = [
+   {
+      id: 1,
+      count: 0,
+   },
+   {
+      id: 2,
+      count: 0,
+   },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [state, setState] = useState(initialState);
+
+   //  total counter
+   const totalCounter = () => {
+      return state.reduce((total, counter) => total + counter.count, 0);
+   };
+
+   // increment
+   const increment = (id) => {
+      const updatedState = state.map((c) => {
+         if (c.id === id) {
+            return {
+               ...c,
+               count: c.count + 1,
+            };
+         }
+         return { ...c };
+      });
+
+      setState(updatedState);
+   };
+
+   // decrement
+   const decrement = (id) => {
+      const updatedState = state.map((c) => {
+         if (c.id === id) {
+            return {
+               ...c,
+               count: c.count - 1,
+            };
+         }
+         return { ...c };
+      });
+      setState(updatedState);
+   };
+
+   return (
+      <div className="App">
+         {state.map((count) => (
+            <Counter
+               key={count.id}
+               id={count.id}
+               count={count.count}
+               increment={increment}
+               decrement={decrement}
+            />
+         ))}
+
+         <Stats count={totalCounter()} />
+      </div>
+   );
 }
 
 export default App;
