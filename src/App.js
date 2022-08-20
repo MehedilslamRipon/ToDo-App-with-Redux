a51c35eb8a5fa395f946cc352570bcc137cc235a
MehediIslamRipon <mehediislamripon@gmail.com>
2022-08-20 17:49:02 +0600

    JSX fixing

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import Footer from './Components/Footer';
 
 export default function App() {
    return (
-      <div class="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
+      <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
          {/* navbar */}
          <Navbar />
 
-         <div class="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+         <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
             {/* header */}
             <Header />
-            <hr class="mt-4" />
+            <hr className="mt-4" />
 
             {/* todo list */}
             <TodoList />
 
-            <hr class="mt-4" />
+            <hr className="mt-4" />
 
             {/* footer */}
             <Footer />
