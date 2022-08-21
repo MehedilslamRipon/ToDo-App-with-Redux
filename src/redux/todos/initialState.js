531e4d086b1766f3ba020b5f754a42a562979867
MehediIslamRipon <mehediislamripon@gmail.com>
2022-08-21 20:05:22 +0600

    created reducer slice for todos

---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,13 @@
+export const initialState = [
+   {
+      id: 1,
+      text: 'Learn Javascript',
+      completed: false,
+   },
+   {
+      id: 2,
+      text: 'Learn Redux',
+      completed: true,
+      color: 'red',
+   },
+];
---
+++ src/redux/todos/reducer.js
@@ -0,0 +1,64 @@
+// import initial state
+import { initialState } from './initialState';
+import {
+   ADDED,
+   ALL_COMPLETED,
+   CLEAR_COMPLETED,
+   COLOR_SELECTED,
+   DELETED,
+   TOGGLED,
+} from './actionTypes';
+
+// create todo id
+const nextTodoId = (todos) => {
+   const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1);
+   return maxId + 1;
+};
+
+const reducer = (state = initialState, action) => {
+   switch (action.type) {
+      case ADDED:
+         return [
+            ...state,
+            {
+               id: nextTodoId(state),
+            },
+         ];
+      case TOGGLED:
+         return state.map((todo) => {
+            if (todo.id !== action.payload) {
+               return todo;
+            }
+            return {
+               ...todo,
+               completed: !todo.completed,
+            };
+         });
+      case COLOR_SELECTED:
+         const { todoId, color } = action.payload;
+         return state.map((todo) => {
+            if (todo.id !== todoId) {
+               return todo;
+            }
+            return {
+               ...todo,
+               color,
+            };
+         });
+      case DELETED:
+         return state.filter((todo) => todo.id !== action.payload);
+      case ALL_COMPLETED:
+         return state.map((todo) => {
+            return {
+               ...todo,
+               completed: true,
+            };
+         });
+      case CLEAR_COMPLETED:
+         return state.filter((todo) => !todo.completed);
+      default:
+         return state;
+   }
+};
+
+export default reducer;
