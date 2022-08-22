785236107d8bc63fe0a8a7d02d991cc6a7c6610f
MehediIslamRipon <mehediislamripon@gmail.com>
2022-08-22 17:40:54 +0600

    redux functionalities creation done

--- src/App.js
+++ src/App.js
@@ -2,26 +2,30 @@ import Navbar from './Components/Navbar';
 import Header from './Components/Header';
 import TodoList from './Components/TodoList';
 import Footer from './Components/Footer';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 export default function App() {
    return (
-      <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
-         {/* navbar */}
-         <Navbar />
+      <Provider store={store}>
+         <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
+            {/* navbar */}
+            <Navbar />
 
-         <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
-            {/* header */}
-            <Header />
-            <hr className="mt-4" />
+            <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+               {/* header */}
+               <Header />
+               <hr className="mt-4" />
 
-            {/* todo list */}
-            <TodoList />
+               {/* todo list */}
+               <TodoList />
 
-            <hr className="mt-4" />
+               <hr className="mt-4" />
 
-            {/* footer */}
-            <Footer />
+               {/* footer */}
+               <Footer />
+            </div>
          </div>
-      </div>
+      </Provider>
    );
 }
